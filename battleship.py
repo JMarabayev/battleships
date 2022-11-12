98d290497b169ee08e7372c4115191c4c9024e73
Zhangir (John) Marabayev <marabayev.z@gmail.com>
2022-11-12 09:28:25 +0000

    Created_GameBoardClass_Defined_PrintBoard_Function

--- battleship.py
+++ battleship.py
@@ -6,3 +6,23 @@
 # 5. If the player sinks all the ships, the computer Loses, otherwise the computer Wins
 # To play the game, please input the grid size and when prompted input the coordinates for your shots
 # Good Luck, Have Fun!
+import random
+
+# Creating the gameboard, "I" was ommited in order to escape confusion
+class GameBoard:
+    def __init__(self,board):
+        self.board=board
+#Creating a dictionary for the coordinates
+    def lettersToNumbers():
+        letters_numbers={"A":0, "B":1, "C":2, "D":3, "E":4, "F":5, "G":6, "H":7, "J":8, "K":9}
+        return letters_numbers
+#Printing the board and Grid
+    def printBoard(self):
+        print(" A B C D E F G H J K")
+        print(" +-+-+-+-+-+-+-+-+-+")
+        row_num= 1
+        for row in self.board:
+            print("%d|%s|"% (row_num, "|".join(row)))
+            row_num+=1
+
+print("Hello World")
