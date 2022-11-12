9460ba41f741c7a9d9b5104044b878b331391c03
Zhangir (John) Marabayev <marabayev.z@gmail.com>
2022-11-12 08:26:55 +0000

    Defied_rules

---
+++ battleship.py
@@ -0,0 +1,8 @@
+# Welcome to my Battleships game. The rules are as follow:
+# 1. The grid size is changeable by the player, it goes without saying that the larger the grid, the more difficult it is to win
+# 2. The computer places 5 ships within the grid and the player tries sink the computer's ships using board coordinates
+# 3. The turn counter is limited to 10 shots per player and at the end of the game if the player does not sink all ships, computer wins
+# 4. Ships that are hit are marked with  "X" and shots missed are marked with "()"
+# 5. If the player sinks all the ships, the computer Loses, otherwise the computer Wins
+# To play the game, please input the grid size and when prompted input the coordinates for your shots
+# Good Luck, Have Fun!
