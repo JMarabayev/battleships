aa217f32e15bb46c2f8338a6b6a90b3a764b5a1f
Zhangir (John) Marabayev <marabayev.z@gmail.com>
2022-11-12 11:45:41 +0000

    Generated_Ships__Added_Hitcount

--- battleship.py
+++ battleship.py
@@ -14,15 +14,48 @@ class GameBoard:
         self.board=board
 #Creating a dictionary for the coordinates
     def lettersToNumbers():
-        letters_numbers={"A":0, "B":1, "C":2, "D":3, "E":4, "F":5, "G":6, "H":7, "J":8, "K":9}
+        letters_numbers={"A":0, "B":1, "C":2, "D":3, "E":4, "F":5, "G":6, "H":7}
         return letters_numbers
 #Printing the board and Grid
     def printBoard(self):
-        print(" A B C D E F G H J K")
-        print(" +-+-+-+-+-+-+-+-+-+")
+        print(" A B C D E F G H")
+        print(" +-+-+-+-+-+-+-+")
         row_num= 1
         for row in self.board:
             print("%d|%s|"% (row_num, "|".join(row)))
             row_num+=1
 
-print("Hello World")
+# Creating Battleship Class (Taken from tutorial mentioned in README)
+class Battleship:
+    def __init__(self, board):
+        self.board=board
+    def generateShips(self):
+        for i in range(5):
+            self.x_row, self.y-column = random.randint(0, 7), random.randint(0, 7)
+            while self.board[self.x_row, self.y_column] == "X":
+                self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
+            self.board[self.x_row,self.y_column]="X"
+        return self.board
+# Getting user Input 
+def getUserInput(self):
+    try:
+        x_row = input("Please Select the row coorinate (1-8)")
+        while x_row not in '12345678':
+            print("You are either out of bounds, or don't know what numbers are")
+            x_row = input("Please Select the X coorinate (A-H)")
+        y_column = input("Please Select the column coordinate (A-H)").upper()
+        while y_column not in 'ABCDEFGH':
+            print("You are either out of bounds, or don't know what Letters are")
+            y_column = input("Please Select the column coordinate (A-H)").upper()
+        return int(x_row)-1, GameBoard.lettersToNumbers()[y_column]
+    except ValueError and KeyError:
+        print("Invalid Choice")
+        return self.getUserInput()
+    #Counting Ships that were hit
+    def HitCount():
+        hit_ships = 0
+        for row in self.board:
+            for column in row:
+                if column == "X":
+                    hit_ships += 1
+        return hit_ships
